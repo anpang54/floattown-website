4120d6f42ea52a0b4d021ebf384c6561b6ad598d
anpang54 <anpangpawit2@gmail.com>
2025-08-26 22:43:44 +0700

    add a scroll animation for the different pages

---
+++ src/script.js
@@ -0,0 +1,34 @@
+
+
+// functions
+
+function id(name) {
+    return document.getElementById(name);
+}
+
+
+// switch page
+
+let currentPage = "home";
+
+function go(page) {
+
+    const start = parseFloat(window.getComputedStyle(id(currentPage)).getPropertyValue("left").slice(0, -2));
+    const end = parseFloat(window.getComputedStyle(id(page)).getPropertyValue("left").slice(0, -2));
+    let value = start;
+
+    let i = 0;
+    let interval = setInterval(() => {
+        value += (end - start) / 100;
+        id("mainscroll").style.transform = `translateX(-${value}px)`;
+        if(i === 100) {
+            clearInterval(interval);
+            id("mainscroll").style.transform = `translateX(-${end}px)`;
+        } else {
+            ++i;
+        }
+    }, 3);
+
+    currentPage = page;
+
+}
