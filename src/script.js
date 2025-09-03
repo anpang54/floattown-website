d0f77837ed9797bb5fbc2c60a9ac9d53e1477bf4
Anpang @ PDS <22132@satitpatumwan.ac.th>
2025-09-03 07:04:14 +0000

    write intro and other content, add blur effect and colored background

--- src/script.js
+++ src/script.js
@@ -7,19 +7,34 @@ function id(name) {
 }
 
 
+// page colors
+
+const colors = {
+    "home": 10,
+    "key": 130,
+    "friends": 60,
+    "pictures": 277,
+    "history": 305,
+    "other-stuff": 204
+};
+
+
 // switch page
 
 let currentPage = "home";
 
 function go(page) {
 
+    // get start/end values for animation
     const start = parseFloat(window.getComputedStyle(id(currentPage)).getPropertyValue("left").slice(0, -2));
     const end = parseFloat(window.getComputedStyle(id(page)).getPropertyValue("left").slice(0, -2));
     let value = start;
 
+    // keep button pressed
     id(`button-${currentPage}`).classList.remove("pressed");
     id(`button-${page}`).classList.add("pressed");
 
+    // do animation
     let i = 0;
     let interval = setInterval(() => {
         value += (end - start) / 100;
@@ -32,6 +47,10 @@ function go(page) {
         }
     }, 1);
 
+    // set color
+    document.body.style.backgroundColor = `hsla(${colors[page]}deg, 100%, 95%, 0.75)`;
+    
+    // set page
     currentPage = page;
 
 }
